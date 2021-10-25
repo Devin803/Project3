bd45d02775485cacfbf9aaac87bc159409244eb1
devin <dinocencio@student.bridgew.edu>
2021-10-24 22:33:40 -0400

    Project 3 progress

--- src/Order.java
+++ src/Order.java
@@ -1,22 +1,28 @@
+import java.util.ArrayList;
+
 public class Order {
 
   private ShippingAddress destination;
   private Customer orderedBy;
+  private ArrayList<merchandiseItem> cartForOrder;
 
   public Order(ShippingAddress dest, Customer cust)
   {
     destination = dest;
     orderedBy = cust;
+    cartForOrder = new ArrayList<merchandiseItem>();
   }
 
   public String getDestination()
   {
     return destination.toString();
   }
-
   public String getOrderer()
   {
     return orderedBy.toString();
+  }
+  public ArrayList<merchandiseItem> getCartForOrder(){
+    return cartForOrder;
   }
 
 }
--- src/merchandiseItem.java
+++ src/merchandiseItem.java
@@ -1,3 +1,21 @@
 public class merchandiseItem{
 
+    private String name;
+    private double price;
+    private ItemType taxibleType;
+
+    public merchandiseItem(String name, double price, ItemType taxibleType) {
+        this.name = name;
+        this.price = price;
+        this.taxibleType = taxibleType;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public ItemType getTaxibleType(){
+        return taxibleType;
+    }
 }
